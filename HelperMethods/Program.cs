5adb7f3c45eea62caf59f6d5e9289476e0d7fc4e
benjie estrella <bench323010@gmail.com>
2016-12-06 21:12:29 +0800

    First commit
    Signed-off-by: benjie estrella <bench323010@gmail.com>

---
+++ HelperMethods/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myvalue = superSecretFormula("World");
+            Console.WriteLine(myvalue);
+            Console.ReadLine();
+        }
+
+        private static string superSecretFormula()
+        {
+            
+            return "hello world.";
+        }
+
+
+        private static string superSecretFormula(string name)
+        {
+
+
+            return String.Format("Hello {0}", name);
+        }
+
+
+           
+    }
+}
